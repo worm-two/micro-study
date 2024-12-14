2a16c8e15ff4ec2a0d52bae80639f18ada4c64c8
xuming <xuming@qq.com>
2024-12-14 13:08:54 +0800

    [初始化]

---
+++ auth-service/src/main/java/gou/xuming/auth/boot/AuthServiceApplication.java
@@ -0,0 +1,10 @@
+package gou.xuming.auth.boot;
+
+/**
+ * @Author: xuming
+ * @Date: 2024:10:26 13:21
+ * @Version: 1.0
+ * @Description: TODO
+ **/
+public class AuthServiceApplication {
+}
---
+++ cache-service/src/main/java/gou/xuming/cache/boot/CacheServiceApplication.java
@@ -0,0 +1,10 @@
+package gou.xuming.cache.boot;
+
+/**
+ * @Author: xuming
+ * @Date: 2024:10:26 13:25
+ * @Version: 1.0
+ * @Description: TODO
+ **/
+public class CacheServiceApplication {
+}
